29741eefa6f6cf8221a5d8561d871af4dc4db200
ManaswiniKundeti <manaswini.kundeti@gmail.com>
2020-08-30 18:24:23 -0400

    Added Instrumentation tests for detailFragment

--- app/src/androidTest/java/com/bb/blockbuster/MovieDetailFragmentTest.kt
+++ app/src/androidTest/java/com/bb/blockbuster/MovieDetailFragmentTest.kt
@@ -2,8 +2,11 @@ package com.bb.blockbuster
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.bb.blockbuster.view.fragment.MovieDetailFragment
 import com.bb.blockbuster.view.fragment.MoviesListFragment
@@ -16,7 +19,31 @@ class MovieDetailFragmentTest {
     @Test
     fun test_start_state() {
         launchFragmentInContainer<MovieDetailFragment>()
-        Espresso.onView(ViewMatchers.withId(R.id.movie_detail_name_text_view))
+
+        onView(withId(R.id.movie_detail_name_text_view))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withId(R.id.movie_detail_name_text_view))
+            .check(ViewAssertions.matches(withText(MOVIE_NAME)))
+
+        onView(withId(R.id.movie_detail_rating_bar))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.vote_count_text_view))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withId(R.id.vote_count_text_view))
+            .check(ViewAssertions.matches(withText(MOVIE_VOTES)))
+
+        onView(withId(R.id.release_date_text_view))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withId(R.id.release_date_text_view))
+            .check(ViewAssertions.matches(withText(MOVIE_RELEASE_DATE)))
+
+        onView(withId(R.id.add_to_cart_button))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withId(R.id.add_to_cart_button))
+            .check(ViewAssertions.matches(withText(ADD_TO_CART_TEXT)))
+
+
+
     }
 }
---
+++ app/src/androidTest/java/com/bb/blockbuster/UITestData.kt
@@ -0,0 +1,8 @@
+package com.bb.blockbuster
+
+const val MOVIE_ID = 11
+const val MOVIE_NAME = "Avengers"
+const val MOVIE_RATING = 6.7f
+const val MOVIE_VOTES = 869
+const val MOVIE_RELEASE_DATE = "2020-08-14"
+const val ADD_TO_CART_TEXT = "Add to Cart ($5.99)"
